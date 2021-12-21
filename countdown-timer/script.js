2cf1039edae3a067229103b0a58cdf6d9b7a9404
Kazi Rasel <kazirasel50@gmail.com>
2021-12-21 13:22:17 +0600

    almost complete simple countdown timer

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,7 +1,32 @@
 // Simple Countdown Timer using JavaScript
-const newYears = '1 Jan 2021';
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+
+const newYears = "21 december 2021";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
+
+    const totalSeconds = (currentDate - newYearsDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+
 }
+
+//initial call
+countdown();
+
+setInterval(countdown, 1000);

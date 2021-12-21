e20ecab9cf49c2537e787b86dd6acef9d0ce0c73
Kazi Rasel <kazirasel50@gmail.com>
2021-12-21 12:31:20 +0600

    working js file

---
+++ countdown-timer/script.js
@@ -0,0 +1,7 @@
+// Simple Countdown Timer using JavaScript
+const newYears = '1 Jan 2021';
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+}
